419944e9391cab40227686f16230cfa8881b265b
Quintus <quintuscardozo13@gmail.com>
2020-05-28 15:52:18 +1000

    Add project files.

---
+++ AddressValidator/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SyslogReceiver
+{
+    class Database
+    {
+        /// <summary>
+        /// Connect to database
+        /// </summary>
+        /// <returns>database object</returns>
+        private static SqlConnection Connect()
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["DB Connection"];
+            SqlConnection db = new SqlConnection(settings.ConnectionString);
+            try
+            {
+                db.Open();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Connection Error: {e.Message}");
+            }
+            return db;
+
+        }
+    }
+}
---
+++ AddressValidator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AddressValidator
+{
+    class Program
+    {
+        const string FILE = @"D:\Work Experience\AddressValidator\20200515_AddressExamples.txt";
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(FILE);
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split("\t");
+                HouseNumber(cols[0]);
+                Console.WriteLine($"Suburb: {cols[1]}");
+                Console.WriteLine($"State: {cols[2]}");
+                Console.WriteLine($"PostCode: {cols[3]}");
+                Console.WriteLine($"Country: {cols[4]}");
+            }
+            Console.ReadLine();
+        }
+
+        static void HouseNumber(string streetCombined)
+        {
+            Match numberCheck = Regex.Match(streetCombined, @"(.*\d[a-z]?)");
+
+            if (numberCheck.Success)
+            {
+                Console.WriteLine($"House Number: {numberCheck.Value}");
+                Console.WriteLine($"Street Name: {streetCombined.Substring(numberCheck.Index + numberCheck.Length)}");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
