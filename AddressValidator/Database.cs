2c08ea5a91d10d7a122eef5487e075e80f6e4b73
Quintus <quintuscardozo13@gmail.com>
2020-05-29 17:27:31 +1000

    added function to retrieve street locality id

--- AddressValidator/Database.cs
+++ AddressValidator/Database.cs
@@ -36,6 +36,23 @@ namespace AddressValidator
             return db;
 
         }
+        private static bool checkRows(SqlCommand query)
+        {
+            try
+            {
+                int count = (int)query.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return false;
+        }
 
         public static bool CheckValid(FieldName field, string value, SqlConnection db)
         {
@@ -63,7 +80,7 @@ namespace AddressValidator
             else if (field == FieldName.StreetName)
             {
                 string streetType = value.Split(' ').Last();
-                string queryStatement = @"SELECT count(street_locality_pid) FROM [PSMA_G-NAF].[dbo].[STREET_LOCALITY] WHERE DIFFERENCE(street_name, @name) = 4 AND street_type_code LIKE '%@type%'";
+                string queryStatement = @"SELECT count(street_locality_pid) FROM [PSMA_G-NAF].[dbo].[STREET_LOCALITY] WHERE DIFFERENCE(street_name, @name) = 4 AND street_type_code LIKE '%' + @type + '%'";
                 SqlCommand query = new SqlCommand(queryStatement, db);
                 query.Parameters.Add(new SqlParameter("@name", SqlDbType.NVarChar) { Value = value });
                 query.Parameters.Add(new SqlParameter("@type", SqlDbType.NVarChar) { Value = streetType });
@@ -72,23 +89,40 @@ namespace AddressValidator
             return false;
         }
 
-        private static bool checkRows(SqlCommand query)
+        public static string GetStreetLocalityId(string state, string locality, string streetName, SqlConnection db)
         {
-            try
-            {
-                int count = (int)query.ExecuteScalar();
-                Console.WriteLine(count);
+            string stateIdStatement = @"SELECT TOP(1) state_pid FROM [PSMA_G-NAF].[dbo].[STATE] 
+                                      WHERE DIFFERENCE(state_abbreviation, @state) > 2 
+                                      ORDER BY DIFFERENCE(state_abbreviation, @state) DESC";
+            SqlCommand stateIdQuery = new SqlCommand(stateIdStatement, db);
+            stateIdQuery.Parameters.Add(new SqlParameter("@state", SqlDbType.NVarChar) { Value = state });
+            int stateId = Convert.ToInt32(stateIdQuery.ExecuteScalar());
 
-                if (count > 0)
+            if (stateId != 0)
+            {
+                string localityIdStatement = @"SELECT TOP(1) locality_pid FROM [PSMA_G-NAF].[dbo].[LOCALITY] 
+                                             WHERE DIFFERENCE(locality_name, @locality) = 4 and state_pid = @stateId 
+                                             ORDER BY DIFFERENCE(locality_name, @locality) DESC";
+                SqlCommand localityIdQuery = new SqlCommand(localityIdStatement, db);
+                localityIdQuery.Parameters.Add(new SqlParameter("@locality", SqlDbType.NVarChar) { Value = locality });
+                localityIdQuery.Parameters.Add(new SqlParameter("@stateId", SqlDbType.Int) { Value = stateId });
+                string localityId = (string)localityIdQuery.ExecuteScalar();
+
+                if (localityId != null)
                 {
-                    return true;
+                    string streetType = streetName.Split(' ').Last();
+                    string streetLocalityIdStatement = @"SELECT TOP(1) street_locality_pid FROM [PSMA_G-NAF].[dbo].[STREET_LOCALITY] 
+                                              WHERE DIFFERENCE(street_name, @name) = 4 AND locality_pid = @localityId AND street_type_code LIKE '%' + @type + '%' 
+                                              ORDER BY DIFFERENCE(street_name, @name) DESC";
+                    SqlCommand streetLocalityIdQuery = new SqlCommand(streetLocalityIdStatement, db);
+                    streetLocalityIdQuery.Parameters.Add(new SqlParameter("@name", SqlDbType.NVarChar) { Value = streetName });
+                    streetLocalityIdQuery.Parameters.Add(new SqlParameter("@localityId", SqlDbType.NVarChar) { Value = localityId });
+                    streetLocalityIdQuery.Parameters.Add(new SqlParameter("@type", SqlDbType.NVarChar) { Value = streetType });
+                    string streetLocalityId = (string)streetLocalityIdQuery.ExecuteScalar();
+                    return streetLocalityId;
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            return false;
+            return null;
         }
     }
 }
--- AddressValidator/Program.cs
+++ AddressValidator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AddressValidator
@@ -10,32 +11,51 @@ namespace AddressValidator
         const string FILE = @"D:\Work Experience\AddressValidator\20200515_AddressExamples.txt";
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines(FILE);
+            string[] lines = File.ReadAllLines(FILE).Skip(1).ToArray();
             SqlConnection db = Database.Connect();
             foreach (string line in lines)
             {
                 string[] cols = line.Split('\t');
                 bool validPostcode = Database.CheckValid(Database.FieldName.Postcode, cols[3], db);
                 bool validState = Database.CheckValid(Database.FieldName.State, cols[2], db);
-                bool locality = Database.CheckValid(Database.FieldName.Locality, cols[1], db);
-                bool validStreet = HouseNumber(cols[0], db);
+                bool validLocality = Database.CheckValid(Database.FieldName.Locality, cols[1], db);
+                string streetName = StreetName(cols[0]);
+                bool validStreet = Database.CheckValid(Database.FieldName.StreetName, streetName, db);
+
+                if (validPostcode && validState && validLocality && validStreet)
+                {
+                    Console.WriteLine(line);
+                    string streetLocalityId = Database.GetStreetLocalityId(cols[2], cols[1], streetName, db);
+
+                    if (streetLocalityId != null)
+                    {
+                        Console.WriteLine(streetLocalityId);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Found");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine("Not Found");
+                }
+                Console.WriteLine("");
             }
             db.Close();
             Console.ReadLine();
         }
 
-        static bool HouseNumber(string streetCombined, SqlConnection db)
+        static string StreetName(string streetCombined)
         {
             Match numberCheck = Regex.Match(streetCombined, @"(.*\d[a-z]?)");
 
             if (numberCheck.Success)
             {
-                string streetName = streetCombined.Substring(numberCheck.Index + numberCheck.Length);
-                bool validState = Database.CheckValid(Database.FieldName.StreetName, streetName, db);
-                Console.WriteLine($"{streetName} {validState}");
-                return validState;
+                return streetCombined.Substring(numberCheck.Index + numberCheck.Length).Trim();
             }
-            return false;
+            return "";
         }
     }
 }
