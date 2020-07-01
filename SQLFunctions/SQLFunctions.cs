2499708351417bcfaa6ec9580d0a2265faaf3ff5
Quintus <quintuscardozo13@gmail.com>
2020-07-01 13:19:58 +1000

    removed namespace for sql functions

--- SQLFunctions/SQLFunctions.cs
+++ SQLFunctions/SQLFunctions.cs
@@ -1,41 +1,38 @@
 ﻿using System;
 using System.Data.SqlTypes;
 
-namespace SQLFunctions
+public class MathFunctions
 {
-    public class SQLFunctions
+    /// <summary>
+    /// Caculate Levenshtein distance (based off: https://gist.github.com/Davidblkx/e12ab0bb2aff7fd8072632b396538560)
+    /// </summary>
+    /// <param name="source">source word</param>
+    /// <param name="target">target word</param>
+    /// <returns>numeric distance between words</returns>
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlInt32 Distance(SqlString source, SqlString target)
     {
-        /// <summary>
-        /// Caculate Levenshtein distance (based off: https://gist.github.com/Davidblkx/e12ab0bb2aff7fd8072632b396538560)
-        /// </summary>
-        /// <param name="source">source word</param>
-        /// <param name="target">target word</param>
-        /// <returns>numeric distance between words</returns>
-        [Microsoft.SqlServer.Server.SqlFunction]
-        public static SqlInt32 Distance(SqlString source, SqlString target)
+        string sourceStr = source.ToString();
+        string targetStr = target.ToString();
+        int sourceLength = sourceStr.Length;
+        int targetLength = targetStr.Length;
+
+        int[,] matrix = new int[sourceLength + 1, targetLength + 1];
+
+        if (sourceLength == 0) return targetLength;
+        if (targetLength == 0) return sourceLength;
+
+        for (int i = 0; i <= sourceLength; matrix[i, 0] = i++) ;
+        for (int j = 0; j <= targetLength; matrix[0, j] = j++) ;
+
+        for (int i = 1; i <= sourceLength; i++)
         {
-            string sourceStr = source.ToString();
-            string targetStr = target.ToString();
-            int sourceLength = sourceStr.Length;
-            int targetLength = targetStr.Length;
-
-            int[,] matrix = new int[sourceLength + 1, targetLength + 1];
-
-            if (sourceLength == 0) return targetLength;
-            if (targetLength == 0) return sourceLength;
-
-            for (int i = 0; i <= sourceLength; matrix[i, 0] = i++);
-            for (int j = 0; j <= targetLength; matrix[0, j] = j++);
-
-            for (int i = 1; i <= sourceLength; i++)
+            for (int j = 1; j <= targetLength; j++)
             {
-                for (int j = 1; j <= targetLength; j++)
-                {
-                    int cost = (sourceStr[j - 1] == targetStr[i - 1]) ? 0 : 1;
-                    matrix[i, j] = Math.Min(Math.Min(matrix[i -1, j] + 1, matrix[i, j - 1] + 1), matrix[i - 1, j - 1] + cost);
-                }
+                int cost = (sourceStr[j - 1] == targetStr[i - 1]) ? 0 : 1;
+                matrix[i, j] = Math.Min(Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1), matrix[i - 1, j - 1] + cost);
             }
-            return matrix[sourceLength, targetLength];
         }
+        return matrix[sourceLength, targetLength];
     }
 }
